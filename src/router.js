fa77351c84897fb82682ae1839fafa2a9c61b7bd
Marilia Yovana <marilia_yovana@hotmail.com>
2019-10-01 13:01:17 -0500

    revisando si es archivo

--- src/router.js
+++ src/router.js
@@ -1,8 +1,40 @@
 // eslint-disable-next-line jest/no-disabled-tests
-import path from 'path';
+const path = require('path');
+const fs = require('fs');
 
-export const routeAbsolute = route => path.resolve(route);
+// Devuelve la ruta absoluta de una ruta
+export const obtenerRutaAbsoluta = (ruta) => {
+  if (path.isAbsolute(ruta) === true) {
+    return ruta;
+  }
+  return path.resolve(ruta);
+};
 
-export const isRouteAbsolute = path.isAbsolute();
+// si el archivo es un markdown (.md) devuelve true sino false
+export const esArchivoMd = route => path.extname(route) === 'md';
 
-export const fileIsMd = route => path.extname(route) === 'md';
+// si es archivo devuelve true, sino false
+export const esArchivo = ruta => fs.lstatSync(ruta).isFile();
+
+export const mdLinks = (ruta, opciones) => {
+  // obtengo la ruta absoluta
+  const rutaAbsoluta = obtenerRutaAbsoluta(ruta);
+
+  // aqui es cuando se revisa si el archivo existe
+  const rutaExiste = fs.existsSync(rutaAbsoluta);
+
+  if (rutaExiste) {
+    // Reviso si la ruta absoluta es archivo o no
+    const esUnArchivo = esArchivo(rutaAbsoluta);
+    // Si es un archivo
+    if (esUnArchivo) {
+  }
+    // Si no es un archivo
+    else {
+
+    }
+    console.log(esUnArchivo);
+  }
+};
+
+mdLinks('/home/marilia/Proyectos/LIM010-fe-md-links/src/app.js');
